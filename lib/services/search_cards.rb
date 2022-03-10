51eb376c78bcce8b9a2fbe39b424e8e237c1b3e8
Angela Cordova <anvicordova@gmail.com>
2022-03-10 10:50:47 -0500

    Add service to search cards

---
+++ lib/services/search_cards.rb
@@ -0,0 +1,48 @@
+require 'pry'
+require_relative '../models/card'
+require_relative '../models/color'
+
+class SearchCards
+  attr_accessor :groups, :filter_set, :filter_colors, :scope
+
+  def initialize(options)
+    @groups = options[:groups]
+    @filter_set = options[:filter_set]
+    @filter_colors = options[:filter_colors]
+    @scope = Card
+  end
+
+  def call
+    if(filter_set)
+      @scope = @scope.where(set: filter_set)
+    end
+
+    if(filter_colors)
+      @scope = @scope
+                  .joins(:colors)
+                  .where(colors: {name: filter_colors})
+                  .group('cards.id')
+                  .having('COUNT(cards.id) = ?', filter_colors.length)
+    end
+
+    if(groups)
+      grouped_cards.each do |k, v|
+        puts "GROUP: #{k.join(" - ")}"
+        puts "CARDS"
+        puts "#{v.pluck(:name).join("\n")}"
+        puts "-------------------"
+      end
+
+      return
+    end
+
+    puts @scope.to_sql
+    puts @scope.pluck(:name)
+  end
+
+  private
+
+  def grouped_cards
+    @scope.order(groups).group_by { |card| groups.map { |g| card.send(g) }}
+  end
+end
