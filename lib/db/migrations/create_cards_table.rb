8d12a3b960eed0a5b756805608840d3382210f7e
Angela Cordova <anvicordova@Angelas-MacBook-Pro.local>
2022-03-10 10:45:10 -0500

    Add set and rarity to cards table

--- lib/db/migrations/create_cards_table.rb
+++ lib/db/migrations/create_cards_table.rb
@@ -8,6 +8,8 @@ class CreateCardsTable < ActiveRecord::Migration[5.2]
     unless ActiveRecord::Base.connection.table_exists?(:cards)
       create_table :cards do |table|
         table.string :name
+        table.string :set
+        table.string :rarity
         table.timestamps
       end
     end
