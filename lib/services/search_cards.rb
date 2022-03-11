faaf22b639e293eaf474838f1832f0362f5ba1bb
Angela Cordova <anvicordova@gmail.com>
2022-03-11 10:36:54 -0500

    Remove presentation code from SearchCards

--- lib/services/search_cards.rb
+++ lib/services/search_cards.rb
@@ -11,39 +11,30 @@ class SearchCards
     @groups = options[:groups]
     @filter_set = options[:filter_set]
     @filter_colors = options[:filter_colors]
-    @scope = Card
+    @scope = Card.all
   end
 
   def call
     @scope = @scope.where(set: filter_set) if filter_set
 
-    if filter_colors
-      @scope = @scope
-               .joins(:colors)
-               .where(colors: { name: filter_colors })
-               .group('cards.id')
-               .having('COUNT(cards.id) = ?', filter_colors.length)
-    end
+    @scope = filter_by_color if filter_colors
 
-    if groups
-      grouped_cards.each do |k, v|
-        puts "GROUP: #{k.join(' - ')}"
-        puts 'CARDS'
-        puts v.pluck(:name).join("\n").to_s
-        puts '-------------------'
-      end
+    return group_cards if groups
 
-      return
-    end
-
-    puts @scope.to_sql
-    puts @scope.pluck(:name)
     @scope
   end
 
   private
 
-  def grouped_cards
+  def group_cards
     @scope.order(groups).group_by { |card| groups.map { |g| card.send(g) } }
+  end
+
+  def filter_by_color
+    @scope
+      .joins(:colors)
+      .where(colors: { name: filter_colors })
+      .group('cards.id')
+      .having('COUNT(cards.id) = ?', filter_colors.length)
   end
 end
