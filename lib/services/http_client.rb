0a6bba4f8ee80ce1da929479036c22e47cb5ec9c
Angela Cordova <anvicordova@gmail.com>
2022-03-12 11:12:22 -0500

    Add faraday/retry and handle better erros in http connection

--- lib/services/http_client.rb
+++ lib/services/http_client.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 require 'faraday'
+require 'faraday/retry'
+require 'faraday/net_http'
 require 'pry'
 require 'logger'
+require 'ostruct'
 
 class HttpClient
   DEFAULT_TIMEOUT = 3 # seconds
 
-  def initialize(url:, params: {}, headers: {})
+  def initialize(url:, params: {}, headers: {}) # rubocop:disable Metrics/MethodLength
     @logger = Logger.new($stdout)
     @connection = Faraday.new(
       url:,
@@ -16,19 +19,28 @@ class HttpClient
       request: { timeout: DEFAULT_TIMEOUT }
     ) do |c|
       c.use Faraday::Response::RaiseError
+      c.request(
+        :retry,
+        max: 3,
+        interval: 0.05,
+        interval_randomness: 0.5,
+        backoff_factor: 2,
+        exceptions: [Faraday::TimeoutError, Faraday::ConnectionFailed, Faraday::ServerError]
+      )
+      c.adapter(:net_http)
     end
   end
 
   def fetch(endpoint:, params: {})
-    begin
-      response = @connection.get(endpoint, params)
-    rescue Faraday::TimeoutError => e
-      @logger.fatal "Request Timeout: #{e}"
-    rescue Faraday::ConnectionFailed => e
-      @logger.fatal "Connection failed: #{e}"
-    rescue Faraday::ServerError => e
-      @logger.fatal "Error while fetching endpoint: #{e}"
-    end
-    response
+    @connection.get(endpoint, params)
+  rescue Faraday::TimeoutError => e
+    @logger.fatal "Request Timeout: #{e}"
+    OpenStruct.new(success: false)
+  rescue Faraday::ConnectionFailed => e
+    @logger.fatal "Connection failed: #{e}"
+    OpenStruct.new(success: false)
+  rescue Faraday::ServerError => e
+    @logger.fatal "Error while fetching endpoint: #{e}"
+    OpenStruct.new(success: false)
   end
 end
